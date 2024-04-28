7a3a349d910cc4a584f3ee200b3f7c8d0e07e625
dbryan17 <dfbryan17@gmail.com>
2024-04-27 22:44:16 -0600

    abstraction and arrow keys handled

--- src/pages/generateCws/grid.jsx
+++ src/pages/generateCws/grid.jsx
@@ -5,13 +5,17 @@ export default function Grid({ height, width }) {
   functionality:
     - highlight the cell you are brightly xxxxx
     - highlight the rest of row or column a duller color xxxxxxx
-    - tab and shift tab to change highlhgited row or col 
+    - tab and shift tab to change highlhgited row or col  xxxxxxxxxxxxx
       tab:
         - if you are in row highlight, it will find the next row "word" that has empty spaces
           - if there are none, it will go to the first column "word" with empty spaces
         - same for column
       shift tab:
       - same for tab but going backwards
+    - arrow keys: 
+      - if it is a direction you are not highlighting (i.e.) up/down with highlighting cols
+        - it switches the direction 
+      - moves to next non black sqaure space that is in that direction, stops at the end 
     
       
   */
@@ -61,33 +65,59 @@ export default function Grid({ height, width }) {
       return;
     }
     // switch to opposite if it is a double click
-
     let isSameCell =
       selectedCell.rowIdx === rowIdx && selectedCell.colIdx === colIdx;
     let newIsRow = isSameCell ? !selectedWord.isRow : selectedWord.isRow;
 
-    let { startIdx, endIdx } = findWord(gridValues, rowIdx, colIdx, newIsRow);
-
-    // dont need this
-    if (!isSameCell) {
-      setSelectedCell({ rowIdx: rowIdx, colIdx: colIdx });
-    }
-
-    setSelectedWord({
-      startIdx: startIdx,
-      endIdx: endIdx,
-      otherIdx: newIsRow ? rowIdx : colIdx,
-      isRow: newIsRow,
-    });
+    handleHighlight(rowIdx, colIdx, newIsRow, false);
+    return;
   };
 
   /////////////
 
-  /*
-  called from handle key down to calculate new highlighted word
-  TODO 
+  /* 
+  helper function to do new highlights 
+  takes rowIdx, colIdx, and if its a row or col,
+  then highlights the new cell, and highlights the new word 
   */
-  const handleWordHighlightChange = (isForward, rowIdx, colIdx) => {
+  const handleHighlight = (
+    rowIdx,
+    colIdx,
+    isRowHighlight,
+    shouldHighlightFirstEmpty
+  ) => {
+    let { word, startIdx, endIdx } = findWord(
+      gridValues,
+      rowIdx,
+      colIdx,
+      isRowHighlight
+    );
+    if (shouldHighlightFirstEmpty) {
+      let firstEmptyIdx = word.indexOf(" ") + startIdx;
+      setSelectedCell({
+        rowIdx: isRowHighlight ? rowIdx : firstEmptyIdx,
+        colIdx: isRowHighlight ? firstEmptyIdx : colIdx,
+      });
+    } else {
+      setSelectedCell({ rowIdx: rowIdx, colIdx: colIdx });
+    }
+    setSelectedWord({
+      startIdx: startIdx,
+      endIdx: endIdx,
+      otherIdx: isRowHighlight ? rowIdx : colIdx,
+      isRow: isRowHighlight,
+    });
+  };
+
+  /*
+  called from handle key down to calculate new highlighted word when the user presses tab or shift tab
+  - works indentical to new york times crossword
+  - if you are on row or col highlight, stick with it
+  - find the next word that has an empty space (for tab) previous for shift tab
+  - if there are none in the rest of the grid for row or col, switch
+    - so if you are find next row, and there are no more rows that have empty spaces, find first col with empty space
+  */
+  const handleWordHighlightChangeForTabs = (isForward, rowIdx, colIdx) => {
     // find the current word
     let { startIdx, endIdx } = findWord(
       gridValues,
@@ -110,34 +140,60 @@ export default function Grid({ height, width }) {
     );
 
     // now to set the new highlight
-    // get the new word
-    let {
-      word,
-      startIdx: newStartIdx,
-      endIdx: newEndIdx,
-    } = findWord(gridValues, newRowIdx, newColIdx, isRowHighlight);
+    handleHighlight(newRowIdx, newColIdx, isRowHighlight, true);
 
-    setSelectedWord({
-      startIdx: newStartIdx,
-      endIdx: newEndIdx,
-      otherIdx: isRowHighlight ? newRowIdx : newColIdx,
-      isRow: isRowHighlight,
-    });
-    // now to set the new cell highlight
-    // want to highlight the first cell in the word that is empty - always has one
-    let firstEmptyIdx = word.indexOf(" ") + newStartIdx;
-    console.log(word);
-    setSelectedCell({
-      rowIdx: isRowHighlight ? newRowIdx : firstEmptyIdx,
-      colIdx: isRowHighlight ? firstEmptyIdx : newColIdx,
-    });
+    return;
+  };
+
+  /* handles arrow key pressses */
+  const handleArrowPress = (isHorizontal, isForward) => {
+    // do nothing if there is no current highlight
+    if (selectedWord.otherIdx === null) {
+      return;
+    }
+    // check if the current highlight does not match the direction
+    if (selectedWord.isRow !== isHorizontal) {
+      // switch direction
+      // get word for new direction
+      handleHighlight(
+        selectedCell.rowIdx,
+        selectedCell.colIdx,
+        isHorizontal,
+        false
+      );
+      return;
+    }
+
+    // going with the current direction of highlight
+    // could do this with a more gnarly turner
+    if (isHorizontal) {
+      let rowIdx = selectedCell.rowIdx;
+      let i = selectedCell.colIdx + (isForward ? +1 : -1);
+      while (isForward ? i < gridValues[0].length : i > -1) {
+        if (gridValues[rowIdx][i] !== "_") {
+          handleHighlight(rowIdx, i, true, false);
+          return;
+        }
+        isForward ? i++ : i--;
+      }
+      // vetical
+    } else {
+      let colIdx = selectedCell.colIdx;
+      let i = selectedCell.rowIdx + (isForward ? +1 : -1);
+      while (isForward ? i < gridValues.length : i > -1) {
+        if (gridValues[i][colIdx] !== "_") {
+          handleHighlight(i, colIdx, false, false);
+          return;
+        }
+        isForward ? i++ : i--;
+      }
+    }
 
     return;
   };
 
   /*
   handles key presses on cells
-  TODO
   */
   const handleKeyDown = (evt, rowIdx, colIdx) => {
     // need to change these because the highlighted cell won't
@@ -167,11 +223,23 @@ export default function Grid({ height, width }) {
       case "Tab":
         // oppposite for shift tab
         if (evt.shiftKey) {
-          handleWordHighlightChange(false, rowIdx, colIdx);
+          handleWordHighlightChangeForTabs(false, rowIdx, colIdx);
         } else {
           // move row or col to next one, if on end of row or col, go to first of the opposite
-          handleWordHighlightChange(true, rowIdx, colIdx);
+          handleWordHighlightChangeForTabs(true, rowIdx, colIdx);
         }
+        return;
+      case "ArrowRight":
+        handleArrowPress(true, true);
+        return;
+      case "ArrowLeft":
+        handleArrowPress(true, false);
+        return;
+      case "ArrowDown":
+        handleArrowPress(false, true);
+        return;
+      case "ArrowUp":
+        handleArrowPress(false, false);
         return;
     }
 
